1190d085419b84007651a213e35277df04407fc6
Stephen Zhou <hi@hyoban.cc>
2023-09-24 16:42:11 +0800

    feat: update deps and lock pnpm version

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,7 @@ import postcss from "postcss"
 import tailwindcss from "tailwindcss"
 import { getIconCollections, iconsPlugin } from "."
 
-test("main", async () => {
+test("main", () => {
   const result = postcss([
     tailwindcss({
       config: {
@@ -43,13 +43,17 @@ test("main", async () => {
 
         background-color: currentColor;
 
-        -webkit-mask: no-repeat center / 100%;
-
-        mask: no-repeat center / 100%;
-
         -webkit-mask-image: var(--svg);
 
         mask-image: var(--svg);
+
+        -webkit-mask-repeat: no-repeat;
+
+        mask-repeat: no-repeat;
+
+        -webkit-mask-size: 100% 100%;
+
+        mask-size: 100% 100%;
 
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='black' d='M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5Z'/%3E%3C/svg%3E\\")
     }
@@ -64,13 +68,17 @@ test("main", async () => {
 
         background-color: currentColor;
 
-        -webkit-mask: no-repeat center / 100%;
-
-        mask: no-repeat center / 100%;
-
         -webkit-mask-image: var(--svg);
 
         mask-image: var(--svg);
+
+        -webkit-mask-repeat: no-repeat;
+
+        mask-repeat: no-repeat;
+
+        -webkit-mask-size: 100% 100%;
+
+        mask-size: 100% 100%;
 
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='none' stroke='black' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M12 5v14m-7-7h14'/%3E%3C/svg%3E\\")
     }
@@ -85,13 +93,17 @@ test("main", async () => {
 
         background-color: currentColor;
 
-        -webkit-mask: no-repeat center / 100%;
-
-        mask: no-repeat center / 100%;
-
         -webkit-mask-image: var(--svg);
 
         mask-image: var(--svg);
+
+        -webkit-mask-repeat: no-repeat;
+
+        mask-repeat: no-repeat;
+
+        -webkit-mask-size: 100% 100%;
+
+        mask-size: 100% 100%;
 
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='black' d='M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5Z'/%3E%3C/svg%3E\\")
     }
@@ -106,13 +118,17 @@ test("main", async () => {
 
         background-color: currentColor;
 
-        -webkit-mask: no-repeat center / 100%;
-
-        mask: no-repeat center / 100%;
-
         -webkit-mask-image: var(--svg);
 
         mask-image: var(--svg);
+
+        -webkit-mask-repeat: no-repeat;
+
+        mask-repeat: no-repeat;
+
+        -webkit-mask-size: 100% 100%;
+
+        mask-size: 100% 100%;
 
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='black' d='M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5Z'/%3E%3C/svg%3E\\")
     }
@@ -160,10 +176,12 @@ test("custom icon", () => {
         width: 1em;
         height: 1em;
         background-color: currentColor;
-        -webkit-mask: no-repeat center / 100%;
-        mask: no-repeat center / 100%;
         -webkit-mask-image: var(--svg);
         mask-image: var(--svg);
+        -webkit-mask-repeat: no-repeat;
+        mask-repeat: no-repeat;
+        -webkit-mask-size: 100% 100%;
+        mask-size: 100% 100%;
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' width='20' height='20'%3E%3Cpath fill='none' stroke='black' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18'/%3E%3C/svg%3E\\")
     }
     "
