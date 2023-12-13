48b07f5c4990ca8f05c34fcf68c0ef19cb047b1d
Stephen Zhou <hi@hyoban.cc>
2023-12-13 09:59:58 +0800

    feat: update @iconify/utils to 2.1.12

--- src/core.test.ts
+++ src/core.test.ts
@@ -17,15 +17,16 @@ test("generateComponent", () => {
   ).toMatchInlineSnapshot(`
     {
       "--svg": "url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='black' d='M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5Z'/%3E%3C/svg%3E\\")",
-      "-webkit-mask": "no-repeat center / 100%",
       "-webkit-mask-image": "var(--svg)",
       "-webkit-mask-position": "center",
+      "-webkit-mask-repeat": "no-repeat",
       "-webkit-mask-size": "contain",
       "background-color": "currentColor",
       "display": "inline-block",
       "height": "1.5em",
-      "mask": "no-repeat center / 100%",
       "mask-image": "var(--svg)",
+      "mask-repeat": "no-repeat",
+      "mask-size": "100% 100%",
       "width": "1.5em",
     }
   `)
--- src/index.test.ts
+++ src/index.test.ts
@@ -43,13 +43,17 @@ test("main", async () => {
 
         background-color: currentColor;
 
-        -webkit-mask: no-repeat center / 100%;
-
-        mask: no-repeat center / 100%;
-
         -webkit-mask-image: var(--svg);
 
         mask-image: var(--svg);
+
+        -webkit-mask-repeat: no-repeat;
+
+        mask-repeat: no-repeat;
+
+        -webkit-mask-size: 100% 100%;
+
+        mask-size: 100% 100%;
 
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='black' d='M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5Z'/%3E%3C/svg%3E\\")
     }
@@ -64,13 +68,17 @@ test("main", async () => {
 
         background-color: currentColor;
 
-        -webkit-mask: no-repeat center / 100%;
-
-        mask: no-repeat center / 100%;
-
         -webkit-mask-image: var(--svg);
 
         mask-image: var(--svg);
+
+        -webkit-mask-repeat: no-repeat;
+
+        mask-repeat: no-repeat;
+
+        -webkit-mask-size: 100% 100%;
+
+        mask-size: 100% 100%;
 
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='none' stroke='black' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M12 5v14m-7-7h14'/%3E%3C/svg%3E\\")
     }
@@ -85,13 +93,17 @@ test("main", async () => {
 
         background-color: currentColor;
 
-        -webkit-mask: no-repeat center / 100%;
-
-        mask: no-repeat center / 100%;
-
         -webkit-mask-image: var(--svg);
 
         mask-image: var(--svg);
+
+        -webkit-mask-repeat: no-repeat;
+
+        mask-repeat: no-repeat;
+
+        -webkit-mask-size: 100% 100%;
+
+        mask-size: 100% 100%;
 
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='black' d='M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5Z'/%3E%3C/svg%3E\\")
     }
@@ -106,13 +118,17 @@ test("main", async () => {
 
         background-color: currentColor;
 
-        -webkit-mask: no-repeat center / 100%;
-
-        mask: no-repeat center / 100%;
-
         -webkit-mask-image: var(--svg);
 
         mask-image: var(--svg);
+
+        -webkit-mask-repeat: no-repeat;
+
+        mask-repeat: no-repeat;
+
+        -webkit-mask-size: 100% 100%;
+
+        mask-size: 100% 100%;
 
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='black' d='M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5Z'/%3E%3C/svg%3E\\")
     }
@@ -160,10 +176,12 @@ test("custom icon", () => {
         width: 1em;
         height: 1em;
         background-color: currentColor;
-        -webkit-mask: no-repeat center / 100%;
-        mask: no-repeat center / 100%;
         -webkit-mask-image: var(--svg);
         mask-image: var(--svg);
+        -webkit-mask-repeat: no-repeat;
+        mask-repeat: no-repeat;
+        -webkit-mask-size: 100% 100%;
+        mask-size: 100% 100%;
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' width='20' height='20'%3E%3Cpath fill='none' stroke='black' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18'/%3E%3C/svg%3E\\")
     }
     "
@@ -198,10 +216,12 @@ test("set collection automatically", () => {
         width: 1em;
         height: 1em;
         background-color: currentColor;
-        -webkit-mask: no-repeat center / 100%;
-        mask: no-repeat center / 100%;
         -webkit-mask-image: var(--svg);
         mask-image: var(--svg);
+        -webkit-mask-repeat: no-repeat;
+        mask-repeat: no-repeat;
+        -webkit-mask-size: 100% 100%;
+        mask-size: 100% 100%;
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='none' stroke='black' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18'/%3E%3C/svg%3E\\")
     }
     "
@@ -249,10 +269,12 @@ test("custom icon collection name", () => {
         width: 1em;
         height: 1em;
         background-color: currentColor;
-        -webkit-mask: no-repeat center / 100%;
-        mask: no-repeat center / 100%;
         -webkit-mask-image: var(--svg);
         mask-image: var(--svg);
+        -webkit-mask-repeat: no-repeat;
+        mask-repeat: no-repeat;
+        -webkit-mask-size: 100% 100%;
+        mask-size: 100% 100%;
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='none' stroke='black' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='m20.25 7.5l-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z'/%3E%3C/svg%3E\\")
     }
     .foo {
@@ -260,10 +282,12 @@ test("custom icon collection name", () => {
         width: 1em;
         height: 1em;
         background-color: currentColor;
-        -webkit-mask: no-repeat center / 100%;
-        mask: no-repeat center / 100%;
         -webkit-mask-image: var(--svg);
         mask-image: var(--svg);
+        -webkit-mask-repeat: no-repeat;
+        mask-repeat: no-repeat;
+        -webkit-mask-size: 100% 100%;
+        mask-size: 100% 100%;
         --svg: url(\\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='none' stroke='black' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M4.26 10.147a60.436 60.436 0 0 0-.491 6.347A48.627 48.627 0 0 1 12 20.904a48.627 48.627 0 0 1 8.232-4.41a60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.57 50.57 0 0 0-2.658-.813A59.905 59.905 0 0 1 12 3.493a59.902 59.902 0 0 1 10.399 5.84a51.39 51.39 0 0 0-2.658.814m-15.482 0A50.697 50.697 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5a.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5'/%3E%3C/svg%3E\\")
     }"
   `)
