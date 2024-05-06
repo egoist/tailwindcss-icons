1782bdfc216e4ea867f0845ec4a077118a94638a
Stephen Zhou <hi@hyoban.cc>
2024-05-06 21:02:26 +0800

    chore: setup example

--- example/tailwind.config.cjs
+++ example/tailwind.config.cjs
@@ -1,6 +1,17 @@
-const { iconsPlugin, dynamicIconsPlugin } = require("../dist/index.cjs");
+const {
+  iconsPlugin,
+  dynamicIconsPlugin,
+  getIconCollections,
+} = require("../dist/index.cjs");
 
 module.exports = {
   content: ["./index.html"],
-  plugins: [iconsPlugin(), dynamicIconsPlugin()],
+  plugins: [
+    iconsPlugin({
+      scale: 10,
+      strokeWidth: 1,
+      collections: getIconCollections(["lucide", "mdi"]),
+    }),
+    dynamicIconsPlugin({ scale: 10 }),
+  ],
 };
